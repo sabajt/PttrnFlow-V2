465aa16de88c4da3358b2b0b09639f0aaae7fd82
John Saba <john.thomas.saba@gmail.com>
2014-04-05 16:14:50 -0500

    create cocos2d v3 project

---
+++ PttrnFlowV2/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  PttrnFlowV2
+//
+//  Created by John Saba on 4/5/14.
+//  Copyright John Saba 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ PttrnFlowV2/Classes/HelloWorldScene.h
@@ -0,0 +1,27 @@
+//
+//  HelloWorldScene.h
+//  PttrnFlowV2
+//
+//  Created by John Saba on 4/5/14.
+//  Copyright John Saba 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+// Importing cocos2d.h and cocos2d-ui.h, will import anything you need to start using Cocos2D v3
+#import "cocos2d.h"
+#import "cocos2d-ui.h"
+
+// -----------------------------------------------------------------------
+
+/**
+ *  The main scene
+ */
+@interface HelloWorldScene : CCScene
+
+// -----------------------------------------------------------------------
+
++ (HelloWorldScene *)scene;
+- (id)init;
+
+// -----------------------------------------------------------------------
+@end
